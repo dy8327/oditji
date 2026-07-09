0f01531499e19457fc6456808ae31844db2fa34e
choiminkwan0 <rhksehd920@gmail.com>
2026-07-09 15:33:02 +0900

    add: 찜 목록 페이지 기본 매핑 추가

--- src/main/java/com/project/oditji/favorite/controller/FavoriteController.java
+++ src/main/java/com/project/oditji/favorite/controller/FavoriteController.java
@@ -1,5 +1,17 @@
 package com.project.oditji.favorite.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/favorite")
 public class FavoriteController {
-    
+
+    @GetMapping("/list")
+    public String favoriteList() {
+
+        return "favorite/favoriteList";
+    }
+
 }
