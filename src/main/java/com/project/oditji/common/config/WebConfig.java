c099bc419a2907f7536f3d22a37c3e2ce21fbbb5
daseol96 <daseolgim18@gmail.com>
2026-07-10 17:43:16 +0900

    update: 상품 등록 이미지 파일 업로드 기능 구현중

--- src/main/java/com/project/oditji/common/config/WebConfig.java
+++ src/main/java/com/project/oditji/common/config/WebConfig.java
@@ -1,5 +1,24 @@
 package com.project.oditji.common.config;
 
-public class WebConfig {
-    
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(
+            ResourceHandlerRegistry registry) {
+
+        registry.addResourceHandler(
+                    "/upload/profile/**")
+                .addResourceLocations(
+                    "file:///C:/oditji/upload/profile/");
+
+        registry.addResourceHandler(
+                    "/upload/product/**")
+                .addResourceLocations(
+                    "file:///C:/oditji/upload/product/");
+    }
 }
