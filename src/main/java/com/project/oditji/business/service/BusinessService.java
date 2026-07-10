ba29b7e33d0be12cb63f0bb84a5d0b522ea69ad5
daseol96 <daseolgim18@gmail.com>
2026-07-10 17:51:42 +0900

    add: 사업자 기능 추가

--- src/main/java/com/project/oditji/business/service/BusinessService.java
+++ src/main/java/com/project/oditji/business/service/BusinessService.java
@@ -1,5 +1,23 @@
 package com.project.oditji.business.service;
 
-public class BusinessService {
-    
+import java.util.List;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import com.project.oditji.business.vo.ActorSearchVO;
+import com.project.oditji.business.vo.BusinessVO;
+import com.project.oditji.business.vo.ContentSearchVO;
+import com.project.oditji.business.vo.GoodsManageVO;
+
+public interface BusinessService {
+
+    BusinessVO getBusinessByMemberNo(long memberNo);
+
+    List<ContentSearchVO> searchContentList(String keyword);
+
+    List<ActorSearchVO> getActorList();
+
+    List<GoodsManageVO> getProductList(long businessNo);
+
+    long registerProduct(GoodsManageVO goodsManageVO, MultipartFile productImage);
 }
