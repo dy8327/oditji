335ac86cb6fbc8cef096c65a8d4dff93dddc4e0b
choiminkwan0 <rhksehd920@gmail.com>
2026-07-15 17:07:16 +0900

    create: 회원 탈퇴시 복구 및 자동 삭제 기능

--- src/main/java/com/project/oditji/OditjiApplication.java
+++ src/main/java/com/project/oditji/OditjiApplication.java
@@ -4,8 +4,10 @@ import org.mybatis.spring.annotation.MapperScan; // <-- 추가 필요
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableCaching // <-- 추가 필요
+@EnableScheduling
 @SpringBootApplication(scanBasePackages = "com.project.oditji") // 스캔 범위를 패키지 전체로 지정
 @MapperScan("com.project.oditji.**.dao") // 스킨 범위를 패키지 전체가 아닌 .dao 폴더 내부로 한정
 public class OditjiApplication {
