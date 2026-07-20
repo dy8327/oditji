3a4cef238618703dbab7b7a73bf9f07fad6329b3
daseol96 <daseolgim18@gmail.com>
2026-07-20 17:32:44 +0900

    create: 결제 테이블 vo 생성

---
+++ src/main/java/com/project/oditji/payment/vo/PaymentVO.java
@@ -0,0 +1,127 @@
+package com.project.oditji.payment.vo;
+
+import java.util.Date;
+
+public class PaymentVO {
+
+    private Long paymentNo;
+    private Long orderNo;
+    private String orderName;
+    private String paymentId;
+    private Long paymentAmount;
+    private String payMethod;
+    private String paymentStatus;
+    private String pgProvider;
+    private String pgTxId;
+    private String paidAt;
+    private String canceledAt;
+    private String cancelReason;
+    private Date createdAt;
+
+    public PaymentVO() {
+    }
+
+    public Long getPaymentNo() {
+        return paymentNo;
+    }
+
+    public void setPaymentNo(Long paymentNo) {
+        this.paymentNo = paymentNo;
+    }
+
+    public Long getOrderNo() {
+        return orderNo;
+    }
+
+    public void setOrderNo(Long orderNo) {
+        this.orderNo = orderNo;
+    }
+
+    public String getOrderName() {
+        return orderName;
+    }
+
+    public void setOrderName(String orderName) {
+        this.orderName = orderName;
+    }
+
+    public String getPaymentId() {
+        return paymentId;
+    }
+
+    public void setPaymentId(String paymentId) {
+        this.paymentId = paymentId;
+    }
+
+    public Long getPaymentAmount() {
+        return paymentAmount;
+    }
+
+    public void setPaymentAmount(Long paymentAmount) {
+        this.paymentAmount = paymentAmount;
+    }
+
+    public String getPayMethod() {
+        return payMethod;
+    }
+
+    public void setPayMethod(String payMethod) {
+        this.payMethod = payMethod;
+    }
+
+    public String getPaymentStatus() {
+        return paymentStatus;
+    }
+
+    public void setPaymentStatus(String paymentStatus) {
+        this.paymentStatus = paymentStatus;
+    }
+
+    public String getPgProvider() {
+        return pgProvider;
+    }
+
+    public void setPgProvider(String pgProvider) {
+        this.pgProvider = pgProvider;
+    }
+
+    public String getPgTxId() {
+        return pgTxId;
+    }
+
+    public void setPgTxId(String pgTxId) {
+        this.pgTxId = pgTxId;
+    }
+
+    public String getPaidAt() {
+        return paidAt;
+    }
+
+    public void setPaidAt(String paidAt) {
+        this.paidAt = paidAt;
+    }
+
+    public String getCanceledAt() {
+        return canceledAt;
+    }
+
+    public void setCanceledAt(String canceledAt) {
+        this.canceledAt = canceledAt;
+    }
+
+    public String getCancelReason() {
+        return cancelReason;
+    }
+
+    public void setCancelReason(String cancelReason) {
+        this.cancelReason = cancelReason;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+}
