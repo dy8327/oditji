1a79c9aa6806f422b520c57b4a3e93c28d2acfe0
DoYoung Ryu <dy8327@gmail.com>
2026-07-20 10:46:56 +0900

    add: 관리자외 관리자페이지 접근 금지 기능 추가

--- src/main/java/com/project/oditji/common/config/WebConfig.java
+++ src/main/java/com/project/oditji/common/config/WebConfig.java
@@ -1,11 +1,35 @@
 package com.project.oditji.common.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import com.project.oditji.common.interceptor.AdminCheckInterceptor;
+
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    private final AdminCheckInterceptor adminCheckInterceptor;
+
+    public WebConfig(
+            AdminCheckInterceptor adminCheckInterceptor) {
+
+        this.adminCheckInterceptor =
+                adminCheckInterceptor;
+    }
+
+    /*
+     * 관리자 페이지 접근 권한 체크
+     */
+    @Override
+    public void addInterceptors(
+            InterceptorRegistry registry) {
+
+        registry.addInterceptor(
+                    adminCheckInterceptor)
+                .addPathPatterns("/admin/**");
+    }
 
         @Override
         public void addResourceHandlers(ResourceHandlerRegistry registry) {
--- src/main/java/com/project/oditji/common/interceptor/AdminCheckInterceptor.java
+++ src/main/java/com/project/oditji/common/interceptor/AdminCheckInterceptor.java
@@ -1,5 +1,48 @@
 package com.project.oditji.common.interceptor;
 
-public class AdminCheckInterceptor {
-    
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+@Component
+public class AdminCheckInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            Object handler) throws Exception {
+
+        HttpSession session = request.getSession(false);
+
+        // 1. 로그인하지 않은 경우
+        if (session == null
+                || session.getAttribute("loginMember") == null) {
+
+            response.sendRedirect(
+                    request.getContextPath() + "/member/login"
+            );
+
+            return false;
+        }
+
+        // 2. 로그인은 했지만 관리자가 아닌 경우
+        String role =
+                (String) session.getAttribute("role");
+
+        if (!"ADMIN".equals(role)) {
+
+            response.sendRedirect(
+                    request.getContextPath() + "/"
+            );
+
+            return false;
+        }
+
+        // 3. 관리자만 통과
+        return true;
+    }
 }
