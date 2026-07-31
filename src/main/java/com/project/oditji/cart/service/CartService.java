a5d88c0ae4354f0a747fd067b6089b06bcccd14c
daseol96 <daseolgim18@gmail.com>
2026-07-31 14:22:26 +0900

    add: 상품 옵션 번호 필드 생성

--- src/main/java/com/project/oditji/cart/service/CartService.java
+++ src/main/java/com/project/oditji/cart/service/CartService.java
@@ -6,33 +6,28 @@ import com.project.oditji.cart.vo.CartItemVO;
 
 public interface CartService {
 
-    List<CartItemVO> getCartItemList(
-            Long memberNo
-    );
+        List<CartItemVO> getCartItemList(
+                        Long memberNo);
 
-    int addCartItem(
-            Long memberNo,
-            Integer productNo,
-            Integer quantity
-    );
+        int addCartItem(
+                        Long memberNo,
+                        Integer productNo,
+                        Long optionNo,
+                        Integer quantity);
 
-    void updateCartItemQuantity(
-            Long memberNo,
-            Long cartItemNo,
-            Integer quantity
-    );
+        void updateCartItemQuantity(
+                        Long memberNo,
+                        Long cartItemNo,
+                        Integer quantity);
 
-    void deleteCartItem(
-            Long memberNo,
-            Long cartItemNo
-    );
+        void deleteCartItem(
+                        Long memberNo,
+                        Long cartItemNo);
 
-    void deleteSelectedCartItems(
-            Long memberNo,
-            List<Long> cartItemNos
-    );
+        void deleteSelectedCartItems(
+                        Long memberNo,
+                        List<Long> cartItemNos);
 
-    int countCartItems(
-            Long memberNo
-    );
+        int countCartItems(
+                        Long memberNo);
 }
