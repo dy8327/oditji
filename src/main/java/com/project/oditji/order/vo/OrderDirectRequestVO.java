260b31fc3fa5656967a4d26c1e3b305552412e7e
daseol96 <daseolgim18@gmail.com>
2026-07-31 14:10:16 +0900

    add: [상품 옵션 기능 추가] 선택한 색상-사이즈 옵션

--- src/main/java/com/project/oditji/order/vo/OrderDirectRequestVO.java
+++ src/main/java/com/project/oditji/order/vo/OrderDirectRequestVO.java
@@ -8,9 +8,37 @@ package com.project.oditji.order.vo;
 public class OrderDirectRequestVO {
 
     private Integer productNo;
+    // [상품 옵션 기능 추가] 선택한 색상-사이즈 옵션
+    private Long optionNo;
+    private String colorName;
+    private String sizeName;
     private Integer quantity;
 
     public OrderDirectRequestVO() {
+    }
+
+    public Long getOptionNo() {
+        return optionNo;
+    }
+
+    public void setOptionNo(Long optionNo) {
+        this.optionNo = optionNo;
+    }
+
+    public String getColorName() {
+        return colorName;
+    }
+
+    public void setColorName(String colorName) {
+        this.colorName = colorName;
+    }
+
+    public String getSizeName() {
+        return sizeName;
+    }
+
+    public void setSizeName(String sizeName) {
+        this.sizeName = sizeName;
     }
 
     public Integer getProductNo() {
